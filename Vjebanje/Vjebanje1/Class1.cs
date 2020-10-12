c677115ccc75bec4c42e677ea7e71ffa5c73d12d
zecevicmato <mato.zecevic7@gmail.com>
2020-10-12 12:23:54 +0200

    Initial commit

---
+++ Vjebanje/Vjebanje1/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vjebanje1
+{
+    public class Dog
+    {
+        int age;
+        string breed;
+        string name;
+
+        public string Bark()
+        {
+            return "Woof!";
+        }
+
+        //seteri- moraju biti public i tipa void su jer ne vracaju nista nego postavljas na odredenu vrijednost
+
+        public void SetAge(int age)
+        {
+            this.age = age;
+        }
+        //geteri- javni tip je onog sto moras vratit
+        public int GetAge()
+        {
+            return this.age;
+        }
+
+        
+
+    }
+}
---
+++ Vjebanje/Vjebanje1/a.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Vjebanje1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine(a);
+
+            Dog rex;
+            rex = new Dog();
+            Console.WriteLine(rex.Bark());
+            rex.SetAge(11);
+            Console.WriteLine(rex.GetAge());
+
+         
+        }
+    }
+}
